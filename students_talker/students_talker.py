dc8c3f2b04dfce796341e8182237cefdf4bc4054
Mateusz Ciesiółka <ciemakat@gmail.com>
2017-12-20 15:05:28 +0100

    State update fix

--- students_talker/students_talker.py
+++ students_talker/students_talker.py
@@ -12,15 +12,17 @@ class StudentsTalker(Talker):
             self.db[e[0]] = e[1]
         dbfile.close()
 
-    def get_answer(self, question):
+    def get_answer(self, question, state):
 	import pdb; pdb.set_trace()
-        if question.encode("utf-8") not in self.db:
+        if question[question].encode("utf-8") not in self.db:
             return {
                 "answer": "Nie potrafię odpowiedzieć. Spróbuj zadać pytanie inaczej.",
                 "score": 0
+				"state_update": {}
             }
 
         return {
-            "answer": self.db[question],
-            "score": 0.7,
+            "answer": self.db[question[question]],
+            "score": 0.5,
+			"state_update": {}
         }
