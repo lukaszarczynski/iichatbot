4e025cdd7e5beb6d0baab94e0751e800ac7307dc
Bartosz Kostka <kostka.bartosz@gmail.com>
2018-01-11 18:32:46 +0100

    word language model

---
+++ word_language_model/README.md
@@ -0,0 +1,3 @@
+Word language model comes from PyTorch example (and it slightly modified).
+https://github.com/pytorch/examples/tree/master/word_language_model
+
---
+++ word_language_model/data.py
@@ -0,0 +1,50 @@
+import os
+import torch
+
+class Dictionary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = []
+
+    def add_word(self, word):
+        if word not in self.word2idx:
+            self.idx2word.append(word)
+            self.word2idx[word] = len(self.idx2word) - 1
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.idx2word)
+
+
+class Corpus(object):
+    def __init__(self, path):
+        self.dictionary = Dictionary()
+        f = open(path)
+        lines = sum([1 for line in f])
+        self.train = self.tokenize(path, 0, 80*lines/100)
+        self.valid = self.tokenize(path, 80*lines/100, 90*lines/100)
+        self.test = self.tokenize(path, 90*lines/100, lines)
+
+    def tokenize(self, path, beg_line, end_line):
+        """Tokenizes a text file."""
+        assert os.path.exists(path)
+        # Add words to the dictionary
+        with open(path, 'r') as f:
+            tokens = 0
+            for line in f.readlines()[beg_line:end_line]:
+                words = line.split() + ['<eos>']
+                tokens += len(words)
+                for word in words:
+                    self.dictionary.add_word(word)
+
+        # Tokenize file content
+        with open(path, 'r') as f:
+            ids = torch.LongTensor(tokens)
+            token = 0
+            for line in f.readlines()[beg_line:end_line]:
+                words = line.split() + ['<eos>']
+                for word in words:
+                    ids[token] = self.dictionary.word2idx[word]
+                    token += 1
+
+        return ids
