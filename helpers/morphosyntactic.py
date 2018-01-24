7e3ff00cc28ee07eb319006e4b4c4a007f87f654
aniakubik <ania.kubik@gmail.com>
2018-01-24 15:05:05 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Kostero/iichatbot""
    This reverts commit 34dfb66edc57b88db88029ad486cad0ae411545e.

--- helpers/morphosyntactic.py
+++ helpers/morphosyntactic.py
@@ -2,18 +2,38 @@
 from __future__ import print_function, unicode_literals, division
 from codecs import open
 
-import sys
+import sys, os
+import cPickle as pickle
+from singleton_decorator import singleton
 
 from helpers.progress_bar import progress_bar
 
 DICT_LEN = 4811854
 
 
-class Morphosyntactic:
+@singleton
+class Morphosyntactic(object):
     """Stores morphosyntactic dictionary"""
-    def __init__(self, dictionary_file_path):
+    def __init__(self, dictionary_file_path, extension=".morph"):
         self.morphosyntactic_dictionary = {}
         self.dictionary_file_path = dictionary_file_path
+        self.extension=extension
+        self._polish_words = None
+
+    def dictionary_stored(self):
+        return os.path.isfile(self.dictionary_file_path + self.extension)
+
+    def load_morphosyntactic_dictionary(self):
+        if self.dictionary_stored():
+            with open(self.dictionary_file_path + self.extension, "rb") as file:
+                self.morphosyntactic_dictionary = pickle.load(file)
+                return self.morphosyntactic_dictionary
+        else:
+            return self.create_morphosyntactic_dictionary()
+
+    def store_morphosyntactic_dictionary(self):
+        with open(self.dictionary_file_path + self.extension, "wb") as file:
+            pickle.dump(self.morphosyntactic_dictionary, file)
 
     def create_morphosyntactic_dictionary(self):
         """Creates dictionary representation from file"""
@@ -30,17 +50,23 @@ class Morphosyntactic:
                 else:
                     self.morphosyntactic_dictionary[word.lower()] = [base_word]
             print("\n", file=sys.stderr)
+        self.store_morphosyntactic_dictionary()
         return self.morphosyntactic_dictionary
 
+    def polish_words(self):
+        if self._polish_words is None:
+            self._polish_words = set(self.get_dictionary().keys())
+        return self._polish_words
+
     def get_dictionary(self):
-        if len(self.morphosyntactic_dictionary) == 0:
-            self.create_morphosyntactic_dictionary()
+        if self.morphosyntactic_dictionary == {}:
+            self.load_morphosyntactic_dictionary()
         return self.morphosyntactic_dictionary
 
 
 if __name__ == "__main__":
     morph = Morphosyntactic("data/polimorfologik-2.1.txt")
-    morph.create_morphosyntactic_dictionary()
+    morph.get_dictionary()
     d = morph.morphosyntactic_dictionary
     print(d["pić"])
     print(d["piła"])
